9218b47d1ac80ffe749edbd42f4fcbba933d1a27
Carlos Nunez <carlos.enm19@gmail.com>
2025-09-14 19:59:41 -0600

    testing modules

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,87 +1,49 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
+resource "aws_eks_cluster" "example" {
+  name = "example"
 
-provider "aws" {
-  region = var.region
-}
-
-# Filter out local zones, which are not currently supported 
-# with managed node groups
-data "aws_availability_zones" "available" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
-
-locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.8.1"
-
-  name = "education-vpc"
-
-  cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
-
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
+  access_config {
+    authentication_mode = "API"
   }
 
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
+  role_arn = aws_iam_role.cluster.arn
+  version  = "1.31"
+
+  vpc_config {
+    subnet_ids = [
+      var.aws_subnet_az1_id,
+      var.aws_subnet_az2_id,
+      var.aws_subnet_az3_id,
+    ]
   }
+
+  # Ensure that IAM Role permissions are created before and deleted
+  # after EKS Cluster handling. Otherwise, EKS will not be able to
+  # properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+  ]
 }
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+resource "aws_iam_role" "cluster" {
+  name = "eks-cluster-example"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
 
-  name    = local.cluster_name
-  kubernetes_version = "1.33"
-
-  endpoint_public_access           = true
-  enable_cluster_creator_admin_permissions = true
-
-  addons = {
-    coredns                = {}
-    eks-pod-identity-agent = {
-      before_compute = true
-    }
-    kube-proxy             = {}
-    vpc-cni                = {
-      before_compute = true
-    }
-  }
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_groups = {
-    example = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["m5.xlarge"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
-  }
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
 }
--- terraform/modules/eks/vars.tf
+++ terraform/modules/eks/vars.tf
@@ -3,3 +3,18 @@ variable "region" {
   type        = string
   default     = "us-west-2"
 }
+
+variable "aws_subnet_az1_id" {
+  description = "The ID of the subnet in availability zone 1"
+  type        = string
+}
+
+variable "aws_subnet_az2_id" {
+  description = "The ID of the subnet in availability zone 2"
+  type        = string
+}
+
+variable "aws_subnet_az3_id" {
+  description = "The ID of the subnet in availability zone 3"
+  type        = string
+}
